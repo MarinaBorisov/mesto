303c9a1fc5c8e16061fef5c1815de2ffa049571c
Marina Borisova <mariborisova02@gmail.com>
2021-10-10 01:23:00 +0300

    To review 2.1

--- script/main.js
+++ script/main.js
@@ -1,12 +1,14 @@
+/*Save elements in global variables*/
 let popup = document.querySelector('.popup')
 let popupOpenButton = document.querySelector('.profile__edit')
 let popupCloseButton = document.querySelector('.popup__close')
 let formElement = document.querySelector('.popup__profile-form');
-let nameInput = document.querySelector('.popup_field_name');
-let descriptionInput = document.querySelector('.popup_field_description');
+let nameInput = document.querySelector('.popup_field_type_name');
+let descriptionInput = document.querySelector('.popup_field_type_description');
 let profileName = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
+/*Declare handlers*/
 function popupOpen() {
   let userName = profileName.textContent.trim();
   let userSubtitle = profileSubtitle.textContent.trim();
@@ -26,6 +28,7 @@ function formSubmit(evnt) {
   popupClose();
 }
 
+/*Add listeners*/
 formElement.addEventListener('submit', formSubmit);
 popupOpenButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
