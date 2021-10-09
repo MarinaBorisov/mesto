4531f434237689c2c5dc55bf11f39e3bc70726c6
Marina Borisova <mariborisova02@gmail.com>
2021-10-10 01:11:50 +0300

    To review 2

--- script/main.js
+++ script/main.js
@@ -1,9 +1,9 @@
 let popup = document.querySelector('.popup')
 let popupOpenButton = document.querySelector('.profile__edit')
 let popupCloseButton = document.querySelector('.popup__close')
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let descriptionInput = document.querySelector('.popup__description');
+let formElement = document.querySelector('.popup__profile-form');
+let nameInput = document.querySelector('.popup_field_name');
+let descriptionInput = document.querySelector('.popup_field_description');
 let profileName = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
@@ -12,27 +12,20 @@ function popupOpen() {
   let userSubtitle = profileSubtitle.textContent.trim();
   nameInput.value = userName;
   descriptionInput.value = userSubtitle;
-  popup.classList.add('popup_state_opened');
+  popup.classList.add('popup_opened');
 }
 
 function popupClose() {
-  popup.classList.remove('popup_state_opened');
+  popup.classList.remove('popup_opened');
 }
 
-popupOpenButton.addEventListener('click', function() {
-  popup.classList.toggle('popup_state_opened');
-});
-
-popupCloseButton.addEventListener('click', function() {
-  popup.classList.toggle('popup_state_opened');
-});
-
-formElement.addEventListener('submit', function(evnt) {
+function formSubmit(evnt) {
   evnt.preventDefault();
   profileName.textContent = nameInput.value;
   profileSubtitle.textContent = descriptionInput.value;
   popupClose();
-});
+}
 
+formElement.addEventListener('submit', formSubmit);
 popupOpenButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
